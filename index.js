0ebb726aac89fd41e61bcdf78cb793be1faca5bd
Pourya <cepsociety@gmail.com>
2023-02-25 20:59:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -24,3 +24,30 @@ function CustomerDetail (CustomerName, CustomerType, Order){
 function CustomerDetail (User){    
   console.log('This is ${User.CustomerName} of ${User.CustomerType} and need ${User.Order}');
 }
+
+
+// Avoid Mental Mapping , Explicit is better than implicit.
+
+// Bad 
+const locations = ["Austin", "New York", "San Francisco"];
+locations.forEach(l => {
+  doStuff();
+  doSomeOtherStuff();
+  // ...
+  // ...
+  // ...
+  // Wait, what is `l` for again?
+  dispatch(l);
+});
+
+// Good
+
+const locations = ["Austin", "New York", "San Francisco"];
+locations.forEach(location => {
+  doStuff();
+  doSomeOtherStuff();
+  // ...
+  // ...
+  // ...
+  dispatch(location);
+});
