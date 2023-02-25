f43c2320388d0da4fdbbb8c13853ef8f467eccd3
Pourya <cepsociety@gmail.com>
2023-02-25 20:59:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -51,3 +51,18 @@ locations.forEach(location => {
   // ...
   dispatch(location);
 });
+
+
+
+// We will read more code than we will ever write. It's important that the code we do write is readable and searchable. By not naming variables that end up being meaningful for understanding our program, we hurt our readers. Make your names searchable. Tools like buddy.js and ESLint can help identify unnamed constants.
+// Bad:
+
+// What the heck is 86400000 for?
+setTimeout(blastOff, 86400000);
+
+// Good:
+
+// Declare them as capitalized named constants.
+const MILLISECONDS_PER_DAY = 60 * 60 * 24 * 1000; //86400000;
+setTimeout(blastOff, MILLISECONDS_PER_DAY);
+
